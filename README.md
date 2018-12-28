714a7c0c4877d1d158a36b13f32d449ab57ef032
Kolesnikov Romanu <kolesnikov.roma.1999@mail.ru>
2018-12-29 01:25:35 +0300

    Initianal commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# My own blockchain and cryptocurrency
+
+[![Build Status](https://travis-ci.org/KolesnikovR/blockchain.svg?branch=master)](https://travis-ci.org/KolesnikovR/blockchain)
+
+## Installation
+
+1. Python 3.7+
+2. pipenv
+
+```
+$ pip install pipenv
+```
+3. Install requirements  
+```
+$ pipenv install
+```
+
+4. Run the server:
+    * `$ pipenv run python blockchain-api.py`
+    * `$ pipenv run python blockchain-api.py -p 5001`
+    * `$ pipenv run python blockchain-api.py --port 5002`
+    
---
+++ blockchain.py
@@ -0,0 +1,106 @@
+import hashlib
+import json
+import requests
+
+from time import time
+from urllib.parse import urlparse
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+        self.nodes = set()
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transaction,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transaction = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_block):
+        last_proof = last_block['proof']
+        last_hash = self.hash(last_block)
+        proof = 0
+        while self.valid_proof(last_proof, proof, last_hash) is False:
+            proof += 1
+
+        return proof
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Invalid URL')
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            last_block_hash = self.hash(last_block)
+            if block['previous_hash'] != last_block_hash:
+                return False
+            if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
+                return False
+            last_block = block
+            current_index += 1
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @staticmethod
+    def valid_proof(last_proof, proof, last_hash):
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "00000"
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
